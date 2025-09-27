af9262f6b06f3efc336a804ec2e35a5c811ffc58
luascriptsROBLOX <borahkokil35@gmail.com>
2025-09-27 14:31:29 +0530

    Update op.lua

--- op.lua
+++ op.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/luascriptsROBLOX/Blox.lua/refs/heads/main/Protected_6073588948818809.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/luascriptsROBLOX/Blox.lua/refs/heads/main/Protected_9333455568019240.lua.txt"))()
